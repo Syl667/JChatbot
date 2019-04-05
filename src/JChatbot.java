485dcd7a009bbee4a5d61fec6bd24a9a359d9dd3
Syl667 <simon_loeoeck@gmx.de>
2019-04-05 22:52:56 +0200

    fix

--- src/JChatbot.java
+++ src/JChatbot.java
@@ -1,32 +1,53 @@
 import java.util.Hashtable;
+import java.util.HashSet;
+import java.util.function.Consumer;
+import java.util.Arrays;
 
-public class JChatbot() {
-    private Hashtable<String, Integer[]> registered_keys;
+public class JChatbot {
+    private Hashtable<String, HashSet<Integer>> registered_keys;
     private Hashtable<Integer, Answer> answers;
 
+    public JChatbot() {
+        registered_keys = new Hashtable<String, HashSet<Integer>>();
+        answers = new Hashtable<Integer, Answer>();
+    }
+
     public void addAnswer(String[][] necessary_keys, String[] optional_keys, Consumer<String> onAnswer) {
-        
+        int id = answers.size();
+        while (answers.containsKey(id)) {
+            id -= 1;
+        }
+        Answer a = new Answer(id, necessary_keys, onAnswer);
+        answers.put(id, a);
+        for (String[] keys: necessary_keys) {
+            for (String key: keys) {
+                if (!registered_keys.containsKey(key)) {
+                    registered_keys.put(key, new HashSet<Integer>());
+                }
+                registered_keys.get(key).add(id);
+            }
+        }
     }
 
     public void calcAnswer(String statement) {
-        statement = statement.toLower().replaceAll("[^a-z0-9]", " ");
+        statement = statement.toLowerCase().replaceAll("[^a-z0-9]", " ");
         while (statement.contains("  ")) {
             statement.replace("  ", " ");
         }
         String[] keys = statement.split(" ");
-        Hashtable<Integer, Float> ids = new Hashtable<Integer, Float>();
+        Hashtable<Integer, Float> probs = new Hashtable<Integer, Float>();
         for (String key: keys) {
             if (registered_keys.containsKey(key)) {
-                for (Integer id: registered_keys.values()) {
-                    if (!ids.containsKey(id)) ids.put(id, 0f);
-                    ids.put(id, ids.get(id) + 1/keys.length());
+                for (Integer id: registered_keys.get(key)) {
+                    if (!probs.containsKey(id)) probs.put(id, 0f);
+                    probs.put(id, probs.get(id) + 1/keys.length);
                 }
             }
         }
     }
 }
 
-public class Answer {
+class Answer {
      private String[][] keys;
 
      public int id;
@@ -43,7 +64,7 @@ public class Answer {
          for (String[] ands: keys) {
              valid = true;
              for (String key: ands) {
-                 if (!input.contains(key)) {
+                 if (!Arrays.asList(input).contains(key)) {
                      valid = false;
                      break;
                  }
